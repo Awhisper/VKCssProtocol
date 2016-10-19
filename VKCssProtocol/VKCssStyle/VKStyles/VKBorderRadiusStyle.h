642cda8219c159769838258c03ea66da44dfa5b5
/ <65301524@qq.com>
2016-10-19 21:26:51 +0800

    radius

---
+++ VKCssProtocol/VKCssStyle/VKStyles/VKBorderRadiusStyle.h
@@ -0,0 +1,14 @@
+//
+//  VKBorderRadiusStyle.h
+//  VKCssProtocolDemo
+//
+//  Created by Awhisper on 2016/10/19.
+//  Copyright © 2016年 baidu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "VKCssStylePch.h"
+
+@interface VKBorderRadiusStyle : NSObject<VKCssStyleProtocol>
+
+@end
---
+++ VKCssProtocol/VKCssStyle/VKStyles/VKBorderRadiusStyle.m
@@ -0,0 +1,32 @@
+//
+//  VKBorderRadiusStyle.m
+//  VKCssProtocolDemo
+//
+//  Created by Awhisper on 2016/10/19.
+//  Copyright © 2016年 baidu. All rights reserved.
+//
+
+#import "VKBorderRadiusStyle.h"
+
+@implementation VKBorderRadiusStyle
+
+
+VK_REGISTE_ATTRIBUTE()
+
++ (NSString *)styleName{
+    return @"border-radius";
+}
+
++ (void)setTarget:(id)target styleValue:(id)value{
+    CGFloat radius = [value VKIdToCGFloat];
+    
+    if (radius <= 0) {
+        return;
+    }
+    
+    if ([target isKindOfClass:[UIView class]]) {
+        UIView *view = (UIView *)target;
+        view.layer.cornerRadius = radius;
+    }
+}
+@end
